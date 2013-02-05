063eadf2ded0fa8a00e664ba9bdcdfcd7526ced0
Roland van Laar <roland@micite.net>
2013-02-05 22:51:34 +0100

    Add check for force-sysegg = True or False.

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,6 +5,10 @@ CHANGES
 0.1 (unreleased)
 ----------------
 
+- Patch code to allow for force-sysegg=false
+
+- Add original code from osc.recipe.sysegg.
+
 - Add buildout and setup.py.
 
 - Added readme, changes and MIT license.
--- src/syseggrecipe/recipe.py
+++ src/syseggrecipe/recipe.py
@@ -2,7 +2,8 @@ import os
 import logging
 import pkg_resources
 
-class Recipe:
+
+class Recipe(object):
     def __init__(self, buildout, name, options):
         self.buildout = buildout
         self.name = name
@@ -19,16 +20,24 @@ class Recipe:
         for egg in eggs:
             try:
                 dist = pkg_resources.require(egg)[0]
-                egg_egg_link = os.path.join(dev_egg_dir,
-                                                '%s.egg-link' % dist.project_name)
+            except pkg_resources.DistributionNotFound:
+                log.warn('No system distribution for %s found.' % egg)
+                if self.force_syseggs():
+                    raise
+
+            else:
+                egg_egg_link = os.path.join(
+                    dev_egg_dir,
+                    '%s.egg-link' % dist.project_name)
                 f = open(egg_egg_link, 'w')
                 f.write(dist.location)
                 f.close()
                 log.info('Using %s for %s' % (dist.location, egg))
-            except pkg_resources.DistributionNotFound:
-                log.warn('No system distribution for %s found.' % egg)
-                if 'force-sysegg' in self.options:
-                    raise
+
         return ()
+
+    def force_syseggs(self):
+        force = self.options.get('force-sysegg', 'false')
+        return force.lower() == 'true'
 
     update = install
