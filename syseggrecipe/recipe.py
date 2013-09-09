fc933fcfc423f847bcc1088175054bcc1129b280
Reinout van Rees <reinout@vanrees.org>
2013-09-09 23:19:40 +0200

    Use a symlink for dirs directly in sys.path dirs.

--- syseggrecipe/recipe.py
+++ syseggrecipe/recipe.py
@@ -1,9 +1,11 @@
-import os
+import glob
 import logging
+import os
 import pkg_resources
 
 
 class Recipe(object):
+
     def __init__(self, buildout, name, options):
         self.buildout = buildout
         self.name = name
@@ -26,13 +28,42 @@ class Recipe(object):
                     raise
 
             else:
-                egg_egg_link = os.path.join(
-                    dev_egg_dir,
-                    '%s.egg-link' % dist.project_name)
-                f = open(egg_egg_link, 'w')
-                f.write(dist.location)
-                f.close()
-                log.info('Using %s for %s' % (dist.location, egg))
+                if egg in dist.location:
+                    # Proper egg instead of a
+                    # /usr/lib/python/dist-packages dir.
+                    egg_egg_link = os.path.join(
+                        dev_egg_dir,
+                        '%s.egg-link' % dist.project_name)
+                    f = open(egg_egg_link, 'w')
+                    f.write(dist.location)
+                    f.close()
+                    log.info('Using %s for %s', dist.location, egg)
+                else:
+                    # Ouch, a some_syspath_dir/EGGNAME dir...
+                    log.debug("Sysegg's location is %s, which is too generic",
+                              dist.location)
+                    link_to_this = os.path.join(dist.location, egg)
+                    if not os.path.exists(link_to_this):
+                        raise RuntimeError(
+                            "Trying {} for sysegg: not found".format(
+                                link_to_this))
+                    log.info("Using %s for %s", link_to_this, egg)
+                    link_file = os.path.join(dev_egg_dir, egg)
+                    if os.path.exists(link_file):
+                        os.remove(link_file)
+                    os.symlink(link_to_this, link_file)
+                    # Also symlink the egg-info files.
+                    all_filenames = os.listdir(dist.location)
+                    egginfo_filenames = [
+                        filename for filename in all_filenames
+                        if filename.endswith('.egg-info')
+                        and filename.startswith(egg)]
+                    for egginfo_filename in egginfo_filenames:
+                        link_to_this = os.path.join(dist.location, 
+                                                    egginfo_filename)
+                        link_file = os.path.join(dev_egg_dir, 
+                                                 egginfo_filename)
+                        os.symlink(link_to_this, link_file)
 
         return ()
 
